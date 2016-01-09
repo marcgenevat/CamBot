a0b9bfe0daca441c7e50015077bc65616281f351
Marc Genevat <mgenevat@gmail.com>
2016-01-09 18:02:37 +0100

    updating ros_control

---
+++ cambot_control/cambot_hw/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/marcgenevat/catkin_ws/src/CamBot/cambot_control/cambot_hw/build/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/marcgenevat/catkin_ws/src/CamBot/cambot_control/cambot_hw/build/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/marcgenevat/catkin_ws/src/CamBot/cambot_control/cambot_hw/build/devel/lib:/home/marcgenevat/catkin_ws/src/CamBot/cambot_control/cambot_hw/build/devel/lib/x86_64-linux-gnu:/home/marcgenevat/catkin_ws/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/home/marcgenevat/catkin_ws/devel/lib:/opt/ros/indigo/lib"
+export PATH="/home/marcgenevat/catkin_ws/src/CamBot/cambot_control/cambot_hw/build/devel/bin:/home/marcgenevat/catkin_ws/devel/bin:/opt/ros/indigo/bin:/usr/lib/x86_64-linux-gnu/qt5/bin:/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
+export PKG_CONFIG_PATH="/home/marcgenevat/catkin_ws/src/CamBot/cambot_control/cambot_hw/build/devel/lib/pkgconfig:/home/marcgenevat/catkin_ws/src/CamBot/cambot_control/cambot_hw/build/devel/lib/x86_64-linux-gnu/pkgconfig:/home/marcgenevat/catkin_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/marcgenevat/catkin_ws/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/marcgenevat/catkin_ws/src/CamBot/cambot_control/cambot_hw/build"
+export PYTHONPATH="/home/marcgenevat/catkin_ws/src/CamBot/cambot_control/cambot_hw/build/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/marcgenevat/catkin_ws/src/CamBot/cambot_control/cambot_hw/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/marcgenevat/catkin_ws/src/CamBot/cambot_control/cambot_hw:$ROS_PACKAGE_PATH"
